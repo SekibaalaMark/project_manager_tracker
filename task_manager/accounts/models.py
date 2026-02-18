dfa4417f8ba3f39afb9ba5d00d032a776137b019
sekibaalamark <markmajje256@gmail.com>
2026-02-18 20:18:12 +0300

    created custom User Model

--- task_manager/accounts/models.py
+++ task_manager/accounts/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+
+    ROLE_CHOICES = (
+        ("OWNER", "Owner"),
+        ("MANAGER", "Manager"),
+        ("MEMBER", "Member"),
+    )
+
+    organization = models.ForeignKey(
+        "organizations.Organization",
+        on_delete=models.CASCADE,
+        related_name="users",
+        null=True,
+        blank=True
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.email
