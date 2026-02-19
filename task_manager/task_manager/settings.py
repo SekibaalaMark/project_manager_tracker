0c8db9e226c27bdf018b8cd9b92781a43847fcd7
sekibaalamark <markmajje256@gmail.com>
2026-02-19 20:47:41 +0300

    configured email sending in the settings

--- task_manager/task_manager/settings.py
+++ task_manager/task_manager/settings.py
@@ -138,3 +138,15 @@ REST_FRAMEWORK = {
     ),
 }
 
+
+
+from decouple import config
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # ← add this if not already present
+
+EMAIL_HOST       = config('EMAIL_HOST')
+EMAIL_PORT       = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER  = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS    = config('EMAIL_USE_TLS', cast=bool)
+
