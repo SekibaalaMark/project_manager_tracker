7861bf96b65604c073c5204b7d6c53164c1d91c5
sekibaalamark <markmajje256@gmail.com>
2026-02-19 19:58:53 +0300

    created  a login view

--- task_manager/accounts/views.py
+++ task_manager/accounts/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import RegisterSerializer
+from .serializers import *
 
 class RegisterView(APIView):
     def post(self, request):
@@ -10,3 +10,31 @@ class RegisterView(APIView):
             user = serializer.save()
             return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+
+        if serializer.is_valid():
+            data = serializer.validated_data
+            user = data["user"]
+
+            return Response({
+                "message": "Login successful",
+                "access": data["access"],
+                "refresh": data["refresh"],
+                "user": {
+                    "id": user.id,
+                    "username": user.username,
+                    "email": user.email,
+                    "role": user.role,
+                    "organization": user.organization.name if user.organization else None
+                }
+            }, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
