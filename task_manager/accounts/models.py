1fc458caf940c3a88648b031940c93d950cb48cc
sekibaalamark <markmajje256@gmail.com>
2026-02-19 20:29:20 +0300

    added a must change password field to user model

--- task_manager/accounts/models.py
+++ task_manager/accounts/models.py
@@ -25,6 +25,8 @@ class CustomUser(AbstractUser):
     )
 
     username = models.CharField(max_length=40,unique=True)
+    must_change_password = models.BooleanField(default=False)
+
 
     def __str__(self):
         return self.username
