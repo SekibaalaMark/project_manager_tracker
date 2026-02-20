7e97de4401f7dff5697b16996e32a3dc7c519743
sekibaalamark <markmajje256@gmail.com>
2026-02-20 12:04:29 +0300

    added email verified field in user model

--- task_manager/accounts/models.py
+++ task_manager/accounts/models.py
@@ -26,6 +26,7 @@ class CustomUser(AbstractUser):
 
     username = models.CharField(max_length=40,unique=True)
     must_change_password = models.BooleanField(default=False)
+    email_verified = models.BooleanField(default=False)
 
 
     def __str__(self):
