076d8b62ef56670806d2cba9bea31d3638f245c8
sekibaalamark <markmajje256@gmail.com>
2026-02-20 12:06:40 +0300

    created a send verification email that contains a verification link that user click and get verified

--- task_manager/accounts/utils.py
+++ task_manager/accounts/utils.py
@@ -24,3 +24,43 @@ Your Organization Team
         [user.email],
         fail_silently=False,
     )
+
+
+
+from django.urls import reverse
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from .tokens import email_verification_token
+
+def send_verification_email(request, user):
+    uid = urlsafe_base64_encode(force_bytes(user.pk))
+    token = email_verification_token.make_token(user)
+
+    verification_url = f"http://127.0.0.1:8000/api/auth/verify-email/{uid}/{token}/"
+
+    subject = "Verify Your Email"
+    text_content = f"""
+Hi {user.username},
+
+Please verify your email by clicking the link below:
+
+{verification_url}
+"""
+
+    html_content = f"""
+    <h2>Email Verification</h2>
+    <p>Hi {user.username},</p>
+    <p>Please click the link below to verify your email:</p>
+    <a href="{verification_url}">Verify Email</a>
+    """
+
+    email = EmailMultiAlternatives(
+        subject,
+        text_content,
+        settings.DEFAULT_FROM_EMAIL,
+        [user.email],
+    )
+    email.attach_alternative(html_content, "text/html")
+    email.send()
