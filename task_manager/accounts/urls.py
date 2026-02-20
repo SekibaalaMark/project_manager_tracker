93b83c92f0c6156c2e7395c6b5b394632ac86af5
sekibaalamark <markmajje256@gmail.com>
2026-02-20 10:58:46 +0300

    added a the set new password view to accounts app urls.py

--- task_manager/accounts/urls.py
+++ task_manager/accounts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("create-users/", CreateOrganizationUserView.as_view()),
+    path("set-password/", SetNewPasswordView.as_view()),
 
 ]
