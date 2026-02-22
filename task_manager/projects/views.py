9651a0f2cf05a6df606040a1c7d37343d1c666d3
sekibaalamark <markmajje256@gmail.com>
2026-02-22 17:01:52 +0300

    created a view for creating a project

--- task_manager/projects/views.py
+++ task_manager/projects/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from accounts.permissions import *
+from .serializers import *
+
+
+class ManagerCreateProjectView(APIView):
+
+    permission_classes = [IsAuthenticated, IsManager]
+
+    def post(self, request):
+        serializer = ProjectCreateSerializer(
+            data=request.data,
+            context={"request": request}
+        )
+
+        if serializer.is_valid():
+            project = serializer.save()
+            return Response(
+                ProjectCreateSerializer(project).data,
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
