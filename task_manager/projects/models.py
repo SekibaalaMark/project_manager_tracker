cd386045b2496da6ed44f7aceed5fec04f614ce1
sekibaalamark <markmajje256@gmail.com>
2026-02-22 16:43:36 +0300

    created a task model

--- task_manager/projects/models.py
+++ task_manager/projects/models.py
@@ -52,3 +52,55 @@ class Project(models.Model):
     
 
 
+class Task(models.Model):
+
+    STATUS_CHOICES = (
+        ("PENDING", "Pending"),
+        ("IN_PROGRESS", "In Progress"),
+        ("COMPLETED", "Completed"),
+    )
+
+    title = models.CharField(max_length=255)
+
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+
+    organization = models.ForeignKey(
+        "accounts.Organization",
+        on_delete=models.CASCADE,
+    )
+
+    assigned_to = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="assigned_tasks"
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="PENDING"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+
+        # 1. Logic Check: Auto-assign the Task's organization to match the Project
+        if self.project and not self.organization:
+            self.organization = self.project.organization
+        
+        # 2. Validation: Ensure they match even if both were provided manually
+        if self.organization != self.project.organization:
+            raise ValueError("Task organization must match the Project's organization.")
+
+
+        super().save(*args, **kwargs)
+        self.project.update_status()
+
+    def __str__(self):
+        return self.title
+
