edb08b19bb3c50ff7be21f94708ad65fdf08b89e
sekibaalamark <markmajje256@gmail.com>
2026-02-22 15:14:13 +0300

    created permission class for Manager Users

--- task_manager/accounts/permissions.py
+++ task_manager/accounts/permissions.py
@@ -5,3 +5,6 @@ class IsOwner(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == "OWNER"
     
+class IsManager(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == "MANAGER"
