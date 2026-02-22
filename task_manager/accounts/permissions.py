14ad2b8d6cb3da57ed42dfdb89693c8a92e5bcbd
sekibaalamark <markmajje256@gmail.com>
2026-02-22 15:41:54 +0300

    created a permission class for both manager or Owners

--- task_manager/accounts/permissions.py
+++ task_manager/accounts/permissions.py
@@ -8,3 +8,8 @@ class IsOwner(BasePermission):
 class IsManager(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role == "MANAGER"
+
+
+class IsOwnerOrManager(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role in ["OWNER", "MANAGER"]
