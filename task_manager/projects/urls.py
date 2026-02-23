8a1fa44e8cfddbc41bae47ff5feff65ef662dd83
sekibaalamark <markmajje256@gmail.com>
2026-02-23 17:16:37 +0300

    added the owner view project view to the projects app urls

--- task_manager/projects/urls.py
+++ task_manager/projects/urls.py
@@ -4,7 +4,9 @@ from .views import *
 urlpatterns = [
     path("create-project/",ManagerCreateProjectView.as_view(),name="manager-create-project",),
     path("create-tasks/",ManagerCreateTaskView.as_view(),name="manager-create-task",),
-    path("<int:id>/update-status/",MemberUpdateTaskStatusView.as_view(),
+    path("<int:id>/update-task-status/",MemberUpdateTaskStatusView.as_view(),
          name="member-update-task-status",),
+    path("owner-projects-summary/",OwnerDashboardView.as_view(),name="owner-dashboard",
+),
     
 ]
