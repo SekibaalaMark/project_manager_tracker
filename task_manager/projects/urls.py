98619d80e383016bf6069c8a40ef10ddd0b036d1
sekibaalamark <markmajje256@gmail.com>
2026-02-23 15:37:53 +0300

    added the create task View to the projects app urls

--- task_manager/projects/urls.py
+++ task_manager/projects/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ManagerCreateProjectView
+from .views import *
 
 urlpatterns = [
-    path("create/",ManagerCreateProjectView.as_view(),name="manager-create-project",),
+    path("create-project/",ManagerCreateProjectView.as_view(),name="manager-create-project",),
+    path("create-tasks/",ManagerCreateTaskView.as_view(),name="manager-create-task",),
 ]
