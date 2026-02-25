1e171e7749b5f1ab3bb8eb98bc9cc7437c911015
sekibaalamark <markmajje256@gmail.com>
2026-02-25 16:42:28 +0300

    included the activity app urls in the project urls

--- task_manager/task_manager/urls.py
+++ task_manager/task_manager/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("accounts.urls")),
     path("api/projects/", include("projects.urls")),
+    path("api/logs/",include('activity.urls')),
 
 
         # JWT
