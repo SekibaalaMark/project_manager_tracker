01ab622af7036587b53d67c74a38a8d84e077178
sekibaalamark <markmajje256@gmail.com>
2026-02-26 11:47:15 +0300

    registered the manager performance view analytics in the projects app urls

--- task_manager/projects/urls.py
+++ task_manager/projects/urls.py
@@ -4,10 +4,11 @@ from .views import *
 urlpatterns = [
     path("create-project/",ManagerCreateProjectView.as_view(),name="manager-create-project",),
     path("create-tasks/",ManagerCreateTaskView.as_view(),name="manager-create-task",),
-    path("<int:id>/update-task-status/",MemberUpdateTaskStatusView.as_view(),
-         name="member-update-task-status",),
+    path("<int:id>/update-task-status/",MemberUpdateTaskStatusView.as_view(),name="member-update-task-status",),
     path("owner-projects-summary/",OwnerDashboardView.as_view(),name="owner-dashboard",),
     path("owner-dashboard-metrics/",OwnerDashboardMetricsView.as_view(),name="owner-dashboard-metrics",),
+    path("owner/analytics/managers/",ManagerPerformanceAnalyticsView.as_view(),name="manager-performance-analytics",),
+
 
     
 ]
