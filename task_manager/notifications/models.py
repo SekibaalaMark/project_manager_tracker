97d427f1b9927aad5936bf9d441a7b265e36bea0
sekibaalamark <markmajje256@gmail.com>
2026-02-26 12:14:17 +0300

    created a notification model

--- task_manager/notifications/models.py
+++ task_manager/notifications/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Notification(models.Model):
+
+    organization = models.ForeignKey(
+        "accounts.Organization",
+        on_delete=models.CASCADE
+    )
+
+    recipient = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="notifications"
+    )
+
+    message = models.TextField()
+
+    is_read = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
