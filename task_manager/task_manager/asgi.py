30f48d4b0e1ce0f046acb5d2cbbb347731cd1a58
sekibaalamark <markmajje256@gmail.com>
2026-02-27 08:45:42 +0300

    configured websockets in asgi

--- task_manager/task_manager/asgi.py
+++ task_manager/task_manager/asgi.py
@@ -14,3 +14,21 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "task_manager.settings")
 
 application = get_asgi_application()
+
+
+import os
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+import notifications.routing
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "task_manager.settings")
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            notifications.routing.websocket_urlpatterns
+        )
+    ),
+})
