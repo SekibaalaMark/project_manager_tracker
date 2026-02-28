c2ee5a4a9a155ea7dfbd20183888f9f4c1f5350f
sekibaalamark <markmajje256@gmail.com>
2026-02-28 14:38:10 +0300

    corrected the reference bug in notifications

--- task_manager/notifications/models.py
+++ task_manager/notifications/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 class Notification(models.Model):
 
     organization = models.ForeignKey(
-        "accounts.Organization",
+        "organizations.Organization",
         on_delete=models.CASCADE
     )
 
